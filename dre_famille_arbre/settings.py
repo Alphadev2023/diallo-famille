553bd5e1aaef1a8330a3c6c5c810cd37584c394b
alphadev2023 <alphadev2023@gmail.com>
2025-06-19 17:24:24 +0100

    modification du fichier settings

--- dre_famille_arbre/settings.py
+++ dre_famille_arbre/settings.py
@@ -14,14 +14,13 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6-nm5azzu(jq$kgdh4xm^9wz(k_qo#c)0gve^d@b7%fp=9j!ix"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +30,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['.onrender.com']
 
-
+if os.environ.get('RENDER') == 'true':
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 ]
 
 # Pour collecter tous les fichiers dans un seul dossier (en prod)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/" 
 # Default primary key field type
